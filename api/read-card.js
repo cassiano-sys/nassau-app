f927438ad74eee1f7b10c42bc6559c314f2fe2d4
cassiano-sys <cassiano@construtoramonreal.com.br>
2026-09-25 22:38:49 -0300

    Update read-card.js

--- api/read-card.js
+++ api/read-card.js
@@ -1,5 +1,6 @@
 import https from 'https'
- 
+ export const config = { maxDuration: 60 }
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
